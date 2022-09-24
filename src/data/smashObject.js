61ff59926fbcbd59a7a698a43269bb2c7e65b9e7
kakintse <kinkatse@mail.adelphi.edu>
2022-09-24 03:29:25 -0400

    Populate seed data pt 2

--- src/data/smashObject.js
+++ src/data/smashObject.js
@@ -34,124 +34,46 @@ const smashObject = {
                     "result": "5th",
                     "wins": "TBA (Ids in an array of other players)",
                     "losses": "TBA (Ids in an array of other players)"
-                }
-            ]
-        },
-        {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
-            "placings": [
+                },
                 {
-                    "tournament_id": 1,
-                    "result": "5th",
+                    "tournament_id": 2,
+                    "result": "1st",
                     "wins": "TBA (Ids in an array of other players)",
                     "losses": "TBA (Ids in an array of other players)"
                 }
             ]
         },
         {
-            "id": 1,
-            "name": "Kakintse",
+            "id": 2,
+            "name": "Nick",
             "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
+            "rank": 2,
+            "characters": ["Link", "Lucina", "Ganondorf"],
             "placings": [
                 {
                     "tournament_id": 1,
-                    "result": "5th",
+                    "result": "1st",
+                    "wins": "TBA (Ids in an array of other players)",
+                    "losses": "TBA (Ids in an array of other players)"
+                },
+                {
+                    "tournament_id": 2,
+                    "result": "3rd",
                     "wins": "TBA (Ids in an array of other players)",
                     "losses": "TBA (Ids in an array of other players)"
                 }
             ]
         },
         {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
+            "id": 3,
+            "name": "Alan",
+            "attendance": "50%",
+            "rank": 3,
+            "characters": ["Lucina", "Zero Suit Samus", "Wolf"],
             "placings": [
                 {
-                    "tournament_id": 1,
-                    "result": "5th",
-                    "wins": "TBA (Ids in an array of other players)",
-                    "losses": "TBA (Ids in an array of other players)"
-                }
-            ]
-        },
-        {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
-            "placings": [
-                {
-                    "tournament_id": 1,
-                    "result": "5th",
-                    "wins": "TBA (Ids in an array of other players)",
-                    "losses": "TBA (Ids in an array of other players)"
-                }
-            ]
-        },
-        {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
-            "placings": [
-                {
-                    "tournament_id": 1,
-                    "result": "5th",
-                    "wins": "TBA (Ids in an array of other players)",
-                    "losses": "TBA (Ids in an array of other players)"
-                }
-            ]
-        },
-        {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
-            "placings": [
-                {
-                    "tournament_id": 1,
-                    "result": "5th",
-                    "wins": "TBA (Ids in an array of other players)",
-                    "losses": "TBA (Ids in an array of other players)"
-                }
-            ]
-        },
-        {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
-            "placings": [
-                {
-                    "tournament_id": 1,
-                    "result": "5th",
-                    "wins": "TBA (Ids in an array of other players)",
-                    "losses": "TBA (Ids in an array of other players)"
-                }
-            ]
-        },
-        {
-            "id": 1,
-            "name": "Kakintse",
-            "attendance": "100%",
-            "rank": 1,
-            "characters": ["Pikachu", "Pokemon Trainer", "Wolf"],
-            "placings": [
-                {
-                    "tournament_id": 1,
-                    "result": "5th",
+                    "tournament_id": 2,
+                    "result": "2nd",
                     "wins": "TBA (Ids in an array of other players)",
                     "losses": "TBA (Ids in an array of other players)"
                 }
