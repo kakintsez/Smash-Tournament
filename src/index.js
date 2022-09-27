feaf5c48414ab6a8a9d07fccf4cc1d0ed9e26106
kakintse <kinkatse@mail.adelphi.edu>
2022-09-27 01:33:51 -0400

    Prepare for npm run eject

--- src/index.js
+++ src/index.js
@@ -4,17 +4,36 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const Root = () => {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
+// const Root = () => {
+//   return (
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   );
+// }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Root />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const Root = () => {
+    return (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  }
+  
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+})
