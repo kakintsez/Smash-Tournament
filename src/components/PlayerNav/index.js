6dbbbf6d9a8c8cd910810574d1deafd3a2f7596a
kakintse <kinkatse@mail.adelphi.edu>
2022-10-07 14:54:59 -0400

    Make player navbar scoll buttons

--- src/components/PlayerNav/index.js
+++ src/components/PlayerNav/index.js
@@ -1,10 +1,30 @@
 import { Route, Link, NavLink, useParams } from "react-router-dom";
 import './PlayerNav.css';
+import { useState } from 'react'
 import PlayerShow from "../PlayerShow";
 import { sortByRanking } from "../../util/functions";
 
 const PlayerNav = ({ players }) => {
-    const { rankList} = sortByRanking(players)
+    const scrollBar = [0,1,2,3]
+    const [scrollDirection, setScrollDirection] = useState(0);
+
+    const handleScrollClickLeft = (e) => {
+        e.preventDefault();
+        if (setScrollDirection === 0) return;
+        setScrollDirection(scrollDirection - 1);
+    }
+
+    const handleScrollClickRight = (e) => {
+        e.preventDefault();
+        if (setScrollDirection === 3) return;
+        setScrollDirection(scrollDirection + 1);
+    }
+
+    console.log(scrollDirection)
+
+    let playerScrollClass = "player-nav-scroll-left"
+
+    const { rankList} = sortByRanking(players);
 
     let playerList = rankList.map(player => {
         if (Object.values(player.results).length === 0) return null
@@ -25,9 +45,11 @@ const PlayerNav = ({ players }) => {
         <div className="players">
             <h4>Sort Option Coming Soon</h4>
             <ul className="players-nav">
+                <button onClick={handleScrollClickLeft} className="scroll-button left">{"<-"}</button>
                 <div className="player-nav-scroll">
                     {playerList}
                 </div>
+                <button onClick={handleScrollClickRight} className="scroll-button right">{"->"}</button>
             </ul>
             <Route path="/Smash-Tournament/players/:playerName">
                 <PlayerShow players={players} />
